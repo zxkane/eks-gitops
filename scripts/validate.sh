9fe93e9d82456ced2380fd8386a4bea46d658697
Kane <843303+zxkane@users.noreply.github.com>
2022-06-11 07:54:45 +0000

    fix: patch the existing sa created by Helm chart external-secrets

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -65,3 +65,15 @@ find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' fil
       exit 1
     fi
 done
+
+nested_flux_kustomization=("./infrastructure/base/sealed-secrets" "./infrastructure/base/external-secrets" "./infrastructure/overlays/development/secrets")
+echo "INFO - Validating nested kustomize overlays"
+for file in ${nested_flux_kustomization[@]};
+  do
+    echo "INFO - Validating kustomization ${file/%$kustomize_config}"
+    kustomize build "${file/%$kustomize_config}" $kustomize_flags | \
+      kubeconform $kubeconform_config
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+      exit 1
+    fi
+done
